a9e68ec58d5da3c39a4d0a6d7eda9c52e16cb910
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-13 13:08:34 -0500

    Add board_rider method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,7 +1,31 @@
 class Ride
-  attr_reader :info
-  
-  def initialize(info)
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue,
+              :rider_log 
 
+  def initialize(info)
+    @name = info[:name]
+    @min_height = info[:min_height]
+    @admission_fee = info[:admission_fee]
+    @excitement = info[:excitement]
+    @total_revenue = 0
+    @rider_log = {}
+
+  end
+  # def charge(visitor)
+  #   @spending_money -= admission_fee
+  #   admission_fee += @total_revenue
+  # end
+
+  def board_rider(visitor)
+    if visitor.preferences.include?(@excitement) && visitor.tall_enough?(min_height)
+      visitor.spending_money - @admission_fee
+      @rider_log.shift
+    else
+      nil
+    end
   end
 end
