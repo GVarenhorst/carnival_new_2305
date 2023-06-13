ce0b47217bde16cdad42e30c1d491fa373afff62
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-13 10:31:18 -0500

    fix: Update @spending_money attribute to produce correct return value

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -7,7 +7,7 @@ class Visitor
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money
+    @spending_money = spending_money.to_s.tr('$', '').to_i
     @preferences = []
   end
 end
